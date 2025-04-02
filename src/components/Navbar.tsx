1f9197789a1cf5be5c535eeb8b409913d57b7363
Rakesh <rakeshcse123@gmail.com>
2025-04-02 13:51:51 +0530

    Added : Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+export function Navbar({ className }: { className?: string }) {
+    const [active, setActive] = useState<string | null>(null);
+    return (
+        <div
+            className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+        >
+            <Menu setActive={setActive}>
+                <Link href={"/"}>
+                    <MenuItem setActive={setActive} active={null} item="About Us">
+                    </MenuItem>
+                </Link>
+                <MenuItem setActive={setActive} active={active} item="Services">
+                    <div className="flex flex-col space-y-4 text-sm">
+                        <HoveredLink href="ardha-padmasana">Ardha Padmasana</HoveredLink>
+                        <HoveredLink href="/bhujangasana">Bhujangasana</HoveredLink>
+                        <HoveredLink href="/halasana">Halasana</HoveredLink>
+                        <HoveredLink href="/natarajasana">Natarajasana</HoveredLink>
+                        <HoveredLink href="/sarvangasana">Sarvangasana</HoveredLink>
+                        <HoveredLink href="/vrikshasana">Vrikshasana</HoveredLink>
+                    </div>
+                </MenuItem>
+                <MenuItem setActive={setActive} active={null} item="Contact Us" />
+            </Menu>
+        </div>
+    );
+}
