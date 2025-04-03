a48cba556ca178e0029b13a8a36b278dad104dbc
Rakesh <rakeshcse123@gmail.com>
2025-04-03 19:47:43 +0530

    added people component

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    domains: ["images.unsplash.com"], // Add Unsplash as a trusted image source
+  },
   /* config options here */
 };
 
